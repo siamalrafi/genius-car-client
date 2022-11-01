ccc9d0ae4c0231535d87aae4cd6905ffb7547c11
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-02 03:55:06 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import img1 from '../../../assets/images/banner/1.jpg'
+import img2 from '../../../assets/images/banner/2.jpg'
+import img3 from '../../../assets/images/banner/3.jpg'
+import img4 from '../../../assets/images/banner/4.jpg'
+import img5 from '../../../assets/images/banner/5.jpg'
+import img6 from '../../../assets/images/banner/6.jpg'
+
+
+
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1}
+            className="w-full" />
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+            <h1 className='font-bold text-5xl text-white'>
+              Affordable <br />
+              Price For Car <br />
+              Servicing
+            </h1>
+          </div>
+
+
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-24 w-2/5 top-1/2">
+            <p className='text-xl text-white'>
+              There are many variations of passages of  available, but the majority have suffered alteration in some form
+            </p>
+          </div>
+
+          <div className="absolute flex justify-start transform -translate-y-1/2 left-24 w-2/5 top-3/4">
+          <button className="btn btn-warning mr-5">Warning</button>
+         
+          <button className="btn btn-outline btn-warning">Warning</button>
+
+
+          </div>
+
+
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 bottom-0 right-5">
+            <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} className="w-full" />
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5">
+            <a href="#slide1" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a>
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={img4} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a>
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
